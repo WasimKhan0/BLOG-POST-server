bc140de4237065ab08825b96c71aed87791582e8
wasimKhan <96934824+WasimKhan0@users.noreply.github.com>
2023-10-04 15:51:46 +0530

    changes

--- Routes/commentRoute.js
+++ Routes/commentRoute.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 
 const Comment = require("../models/Comment");
-const verifyToken = require("../VerifyToken");
+const verifyToken = require("../verifyToken");
 
 
 //create comment
